5efc176b1c973f44f11922136e513c495d3c2d7b
Julia Fong <Juliamzfong@gmail.com>
2020-03-28 13:04:33 -0500

    Add Lifecycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  ViewEncapsulation, 
+  OnChanges,
+  SimpleChanges,
+  DoCheck, 
+  AfterContentInit, AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked, OnDestroy,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,11 +18,49 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // None or Native is an option
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+  OnInit, 
+  OnChanges, 
+  DoCheck, 
+  AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy {
   @Input('srvElement') element: {type: string, name: string, content: string};
-  constructor() { }
+  @Input() name: string;
+  @ViewChild('heading', {static: true}) header: Element
+
+  constructor() { 
+    console.log('construcrow called!')
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("onchanges called")
+    console.log(changes);
+  }
 
   ngOnInit(): void {
+    console.log('ngonit!')
+    console.log("this.header.nativeElement.textContent")
+    
+  }
+  ngDoCheck() {
+    console.log("ngdocheckkkk")
+  }
+  ngAfterContentInit() {
+    console.log('contentinit hitttingggggg')
+  }
+  ngAfterContentChecked() {
+    console.log('afterContent hits itt!!!')
+  }
+  ngAfterViewInit(){
+    console.log('afterview inittttt')
+    console.log("'text content' +this.header.nativeElement.textContent")
+  }
+  ngAfterViewChecked() {
+    console.log('heeelllooo afterview check')
+  }
+  ngOnDestroy() {
+    console.log('destroy it bro') 
+    
   }
 
 }
